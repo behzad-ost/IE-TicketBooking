509ed1029bce827389c2f559e2d3bc617059ca06
Seyyed Ali Akhavani <sa.akhavani@gmail.com>
2017-02-10 14:57:46 +0330

    Add ServerTransceiver.

---
+++ src/main/java/common/ServerTransceiver.java
@@ -0,0 +1,49 @@
+package common;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Created by ali on 2/10/17.
+ */
+public class ServerTransceiver {
+    private ServerSocket serverSocket;
+    private Socket socket;
+
+    public ServerTransceiver(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+    }
+
+    public void accept() throws IOException {
+        socket = serverSocket.accept();
+        System.out.println(socket);
+    }
+
+    public String receive() throws IOException {
+        String res = "";
+        BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        String line;
+        while((line = input.readLine()) != null) {
+            res+= line +'\n';
+            if(!input.ready())
+                break;
+        }
+        return res;
+    }
+
+    public void send(String data) throws IOException {
+        PrintStream output = new PrintStream(socket.getOutputStream());
+        output.println(data);
+        output.flush();
+    }
+
+    public void close() throws IOException {
+        this.socket.close();
+        this.serverSocket.close();
+    }
+
+}
--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -1,4 +1,5 @@
 package server;
+import common.ServerTransceiver;
 import common.Transceiver;
 import query.ClientSearchQuery;
 import query.CommandHandler;
@@ -12,33 +13,39 @@ import java.net.Socket;
 public class Server {
 
     public static void main(String[] args) throws IOException {
-        ServerSocket server = new ServerSocket(8081);
         Transceiver transceiver = new Transceiver("188.166.78.119", 8081);
+//        ServerSocket server = new ServerSocket(8081);
+        ServerTransceiver server = new ServerTransceiver(8081);
         System.out.println("Server is running");
+        server.accept();
+//        Socket clientSocket = server.accept();
 
-        Socket clientSocket = server.accept();
-        String request = receive(clientSocket);
+//        String request = receive(clientSocket);
+        String request = server.receive();
         System.out.println("request: " + request);
+
         transceiver.send(request);
         String helperResponse = transceiver.receive();
         System.out.println("helper :\n" + helperResponse);
 
+        server.send(helperResponse);
 
-        CommandHandler ch = new CommandHandler(request);
-        ch.parseCommand();
-        String commandType = ch.getCommandType();
-        switch (commandType) {
-            case "search":
-//                ClientSearchQuery csq = new ClientSearchQuery();
-                System.out.println("search!!");
-                break;
-            case "reserve":
-                System.out.println("reserve!");
-                break;
-            case "finalize":
-                System.out.println("finalize!");
-                break;
-        }
+//        CommandHandler ch = new CommandHandler(request);
+//        ch.parseCommand();
+//        String commandType = ch.getCommandType();
+//        switch (commandType) {
+//            case "search":
+////                ClientSearchQuery csq = new ClientSearchQuery();
+//                System.out.println("search!!");
+//                break;
+//            case "reserve":
+//                System.out.println("reserve!");
+//                break;
+//            case "finalize":
+//                System.out.println("finalize!");
+//                break;
+//        }
+
 
 
 //        PrintStream output = new PrintStream(clientSocket.getOutputStream());
@@ -47,19 +54,5 @@ public class Server {
 //        output.close();
 
         server.close();
-    }
-
-    private static String receive(Socket socket) throws IOException {
-        String res = "";
-        BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        String line;
-        while((line = input.readLine()) != null) {
-
-//            System.out.println("data from client: " + line);
-            res+= line +'\n';
-            if(!input.ready())
-                break;
-        }
-        return res;
     }
 }

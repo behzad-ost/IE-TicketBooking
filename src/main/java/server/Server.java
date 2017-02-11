b7dd79b1190e9552abe4277ebab089772c397b0c
Seyyed Ali Akhavani <sa.akhavani@gmail.com>
2017-02-11 15:20:59 +0330

    Add bad command handler

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -24,54 +24,44 @@ public class Server {
                 break;
             System.out.println("request: " + request);
 
-//            transceiver.send(request);
-//            String helperResponse = transceiver.receive();
-//            System.out.println("helper :\n" + helperResponse);
-//
-//            server.send(helperResponse);
+            CommandHandler ch = new CommandHandler(request);
+            ch.parseCommand();
+            String commandType;
+            try {
+                commandType = ch.getCommandType();
+            } catch (ArrayIndexOutOfBoundsException ex) {
+                server.send("Haroomi Dorost Type Kon!");
+                continue;
+            }
 
-        CommandHandler ch = new CommandHandler(request);
-        ch.parseCommand();
-        String commandType;
-        try {
-            commandType = ch.getCommandType();
-        } catch (ArrayIndexOutOfBoundsException ex) {
-            server.send("Haroomi Dorost Type Kon!");
-            continue;
-        }
+            switch (commandType) {
+                case "search":
+                    System.out.println("search!!");
+                    ClientSearchQuery csq = ch.createSearchQuery();
+                    String response = manager.search(csq);
+                    server.send(response);
+                    break;
+                case "reserve":
+                    System.out.println("reserve!");
+                    ClientReserveQuery crq = ch.createReserveQuery();
 
-        switch (commandType) {
-            case "search":
-                System.out.println("search!!");
-                ClientSearchQuery csq = ch.createSearchQuery();
-                String response = manager.search(csq);
-                server.send(response);
-                break;
-            case "reserve":
-                System.out.println("reserve!");
-                ClientReserveQuery crq = ch.createReserveQuery();
-
-                int numOfPeople;
-                numOfPeople = crq.getNumOfPeople();
-                for (int i = 0; i < numOfPeople; i++) {
-                    String personInfo = server.receive();
-                    System.out.println("person: " + personInfo);
-                    crq.addPerson(personInfo);
-                }
-                System.out.println("end!");
-                manager.makeReservation(crq);
-                break;
-            case "finalize":
-                System.out.println("finalize!");
-                ClientFinalizeQuery cfq = ch.createFinalizeQuery();
-                break;
-        }
-
-
-//        PrintStream output = new PrintStream(clientSocket.getOutputStream());
-//        output.println("behx");
-//        output.flush();
-//        output.close();
+                    int numOfPeople;
+                    numOfPeople = crq.getNumOfPeople();
+                    for (int i = 0; i < numOfPeople; i++) {
+                        String personInfo = server.receive();
+                        System.out.println("person: " + personInfo);
+                        crq.addPerson(personInfo);
+                    }
+                    System.out.println("end!");
+                    manager.makeReservation(crq);
+                    break;
+                case "finalize":
+                    System.out.println("finalize!");
+                    ClientFinalizeQuery cfq = ch.createFinalizeQuery();
+                    break;
+                default:
+                    server.send("Haroomi Dorost Type Kon!");
+            }
         }
         server.close();
     }

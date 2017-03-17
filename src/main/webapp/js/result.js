65dc634f609ba5ba5cb202096d8f84f4dbe00ad6
Seyyed Ali Akhavani <sa.akhavani@gmail.com>
2017-03-17 19:57:11 +0330

    Add more search filters to result.html page

--- src/main/webapp/js/result.js
+++ src/main/webapp/js/result.js
@@ -1,37 +1,43 @@
 /**
  * Created by ali on 3/17/17.
  */
-
-function sort(type) {
-    var a, i, j;
+function filter() {
+    var seatClass = document.getElementById("seat-classes").value;
+    var airlineName = document.getElementById("airline-codes").value;
+    var sortType = document.getElementById("sort-type").value;
+    
     var ticketList = document.getElementById("all-tickets");
     var tickets = ticketList.childNodes;
     var ticketsArr = [];
     ticketsArr = removeJunks(tickets);
     ticketList.innerHTML = "";
+    
+    ticketsArr = initializeTickets(ticketsArr);    
 
-    for (i = 0; i < ticketsArr.length; i++) {
-        var price1 = getPriceOfDiv(ticketsArr[i]);
-        for(j = i + 1; j < ticketsArr.length; j++) {
-            var price2 = getPriceOfDiv(ticketsArr[j]);
-            if (type == 'asc') {
-                if (Number(price1) > Number(price2)) {
-                    var tmp = ticketsArr[i];
-                    ticketsArr[i] = ticketsArr[j];
-                    ticketsArr[j] = tmp;
-                }
-            } else {
-                if (Number(price1) < Number(price2)) {
-                    var tmp = ticketsArr[i];
-                    ticketsArr[i] = ticketsArr[j];
-                    ticketsArr[j] = tmp;
-                }                
+    if (sortType == 'صعودی')
+        ticketsArr = sort(ticketsArr, "asc");
+    else
+        ticketsArr = sort(ticketsArr, "desc");
+    
+    if (seatClass != 'همه') {
+        for (var i in ticketsArr) {
+            var seatName = getSeatClassOfDiv(ticketsArr[i]);
+            if (seatName != seatClass) {
+                ticketsArr[i].style.display = "none";
             }
         }
     }
     
+    if (airlineName != 'همه') {
+        for (var i in ticketsArr) {
+            var airline = getAirlineNameOfDiv(ticketsArr[i]);
+            if (airline != airlineName) {
+                ticketsArr[i].style.display = "none";
+            }
+        }
+    }    
+    
     appendTickets(ticketsArr, ticketList);
-    console.log("ez");
 }
 
 function appendTickets(tickets, ticketList) {
@@ -48,6 +54,22 @@ function getPriceOfDiv(ticketsArrElement) {
     return price;
 }
 
+function getSeatClassOfDiv(ticketsArrElement) {
+    var seatClass;
+    var seatClassDiv = ticketsArrElement.getElementsByClassName("plane-class");
+    var infoDiv = seatClassDiv[0].getElementsByTagName("i")
+    seatClass = infoDiv[5].innerHTML;
+    return seatClass;
+}
+
+function getAirlineNameOfDiv(ticketsArrElement) {
+    var airlineName;
+    var airlineNameDiv = ticketsArrElement.getElementsByClassName("info");
+    var infoDiv = airlineNameDiv[0].getElementsByTagName("i")
+    airlineName = infoDiv[1].innerHTML;
+    return airlineName;
+}
+
 function removeJunks(arr) {
     var result = [];
     for (var i in arr) {
@@ -56,4 +78,35 @@ function removeJunks(arr) {
         }
     }
     return result;
+}
+
+function sort(ticketsArr, type) {
+    for (i = 0; i < ticketsArr.length; i++) {
+        var price1 = getPriceOfDiv(ticketsArr[i]);
+        for(j = 0; j < ticketsArr.length; j++) {
+            var price2 = getPriceOfDiv(ticketsArr[j]);
+            
+            if (type == 'asc') {
+                if (Number(price1) < Number(price2)) {
+                    var tmp = ticketsArr[i];
+                    ticketsArr[i] = ticketsArr[j];
+                    ticketsArr[j] = tmp;
+                }
+            } else {
+                if (Number(price1) > Number(price2)) {
+                    var tmp = ticketsArr[i];
+                    ticketsArr[i] = ticketsArr[j];
+                    ticketsArr[j] = tmp;
+                }                
+            }
+        }
+    }
+    return ticketsArr;
+}
+
+function initializeTickets(ticketsArr) {
+    for (var i in ticketsArr) {
+        ticketsArr[i].style.display = "";
+    }
+    return ticketsArr;
 }
